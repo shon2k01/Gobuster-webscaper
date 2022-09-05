eec77f50387bb847f6f46d506d56b7f32f9ce7a8
shon2k01 <99765978+shon2k01@users.noreply.github.com>
2022-09-05 14:02:40 +0300

    Update webScraper.py
    -fixed some buggs
    -limited concurrency

--- webScraper.py
+++ webScraper.py
@@ -13,39 +13,16 @@ from bs4 import BeautifulSoup
 
 #parse page html (soup)
 def parse(soup):
-    #here you will search for whatever you need
+    #here you will parse for whatever you need
     #In this example we search for text boxes and return their 'name' property
-    found = [input['name'] for input in  soup.find_all('input') if (input['type'] in ['text','password','email'])]
+    try:
+        found = [_input['name'] for _input in  soup.find_all('input') if _input['type'] in ['text','password','email']]
+
+    #catch errors from inputs which don't have a 'type' attribute
+    except KeyError as e:
+        return None
+
     return found
-
-#get site's html and pass it to parser
-async def get_site_content(url):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url) as resp:
-                text = await resp.read()
-
-            loop = asyncio.get_event_loop()
-            soup = BeautifulSoup(text.decode('utf-8'), 'lxml')
-            items = await loop.run_in_executor(None,parse,soup) #pass html to parse function
-            print(f"Found in: {url}")
-            print(f'{items}')
-        except aiohttp.InvalidURL as e:
-            print(f'Invalid url on: {e}')
-
-
-#process live output from cmd process
-async def process_live_output(process):
-    # Wait for the subprocess to finish
-    #await stdout !=0
-    while True:
-        line = await process.stdout.readline()
-        if 'Started: ' not in line.decode():
-            url = line.decode().strip().replace('Found: ', '')
-            if(url != ''):
-                await get_site_content('https://' + url)
-        if not line:
-            break
 
 #create a process for command and pass it to processing function
 async def run_command_shell(command):
@@ -60,6 +37,44 @@ async def run_command_shell(command):
     #process live output
     await process_live_output(process)
 
+# Initialize a semaphore object with a limit
+#this will limit the concurrency to make sure we don't over-request
+limit = asyncio.Semaphore(4)
+
+#process live output from cmd process
+async def process_live_output(process):
+
+    # Wait for the subprocess to finish
+    #await stdout !=0
+    while True:
+        line = await process.stdout.readline()
+        if 'Started: ' not in line.decode():
+            url = line.decode().strip().replace('Found: ', '')
+            if url != '':
+                async with limit:
+                    await get_site_content('https://' + url)
+                    # When workers hit the limit, they'll wait for a second
+                    # before making more requests.
+                    if limit.locked():
+                        print("Concurrency limit reached, waiting ...")
+                        await asyncio.sleep(1)
+        if not line:
+            break
+
+#get site's html and pass it to parser
+async def get_site_content(url):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url) as resp:
+                text = await resp.read()
+
+            loop = asyncio.get_event_loop()
+            soup = BeautifulSoup(text.decode('utf-8'), 'lxml')
+            items = await loop.run_in_executor(None,parse,soup) #pass html to parse function
+            if items is not None and items != []:
+                print(f"Found in: {url}\n{items}")
+        except aiohttp.ClientConnectionError as e:
+            print(f'Invalid url on: {e}')
 
 #Run tasks asynchronously using asyncio and return results
 async def run_asyncio_commands(tasks):
@@ -91,4 +106,3 @@ async def main():
 
 
 asyncio.get_event_loop().run_until_complete(main())
-
